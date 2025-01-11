0bae3d8df3881b19f0f974494441bf9cf23e0b70
kirill160 <81866092+kirill160@users.noreply.github.com>
2025-01-11 21:38:14 +0300

    Delete basics/string/optiona directory

--- basics/string/optiona/optiona/test/Test.java
+++
@@ -1,9 +0,0 @@
-package basics.string.optiona.test;
-
-import basics.string.optiona.task.ChangeLetter;
-
-public class Test {
-    public static void main(String[] args) {
-        new ChangeLetter().changeLetter(3, 'k');
-    }
-}
