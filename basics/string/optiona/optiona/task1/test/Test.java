94074c3140dea4d99dd355f3a87b3945a45d2a3f
kirill160 <81866092+kirill160@users.noreply.github.com>
2025-01-12 21:45:13 +0300

    Add files via upload

---
+++ basics/string/optiona/optiona/task1/test/Test.java
@@ -0,0 +1,9 @@
+package basics.string.optiona.task1.test;
+
+import basics.string.optiona.task1.task.ModificationText;
+
+public class Test {
+    public static void main(String[] args) {
+        new ModificationText().changeLetter(3, 'k');
+    }
+}
