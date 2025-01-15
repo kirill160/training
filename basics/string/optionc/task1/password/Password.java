db9929cbc84c1b41dcfeea02224187d312319842
kirill160 <81866092+kirill160@users.noreply.github.com>
2025-01-16 01:45:25 +0300

    Add files via upload

--- basics/string/optionc/task1/password/Password.java
+++ basics/string/optionc/task1/password/Password.java
@@ -1,9 +1,14 @@
 package basics.string.optionc.task1.password;
 
-import java.io.Console;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Password {
-    public void checkPassword(){
+    public boolean checkPassword(String password) {
 
+        Matcher matcher = Pattern.compile("^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])[A-Za-z_0-9]{10,}").matcher(password);
+
+        return matcher.matches();
     }
 }
---
+++ basics/string/optionc/task1/test/TestPassword.java
@@ -0,0 +1,11 @@
+package basics.string.optionc.task1.test;
+
+import basics.string.optionc.task1.password.Password;
+
+public class TestPassword {
+    public static void main(String[] args) {
+        String password = "asd1sdLay_asbc";
+        System.out.println(new Password().checkPassword(password));
+        System.out.println(password.length());
+    }
+}
