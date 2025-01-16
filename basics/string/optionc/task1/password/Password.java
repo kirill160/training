c29a91aaf37565917d549ae5adffad32a7248f0b
kirill160 <81866092+kirill160@users.noreply.github.com>
2025-01-16 14:15:24 +0300

    Add files via upload

--- basics/string/optionc/task1/password/Password.java
+++ basics/string/optionc/task1/password/Password.java
@@ -1,14 +1,11 @@
 package basics.string.optionc.task1.password;
 
 
-import java.util.regex.Matcher;
+
 import java.util.regex.Pattern;
 
 public class Password {
     public boolean checkPassword(String password) {
-
-        Matcher matcher = Pattern.compile("^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])[A-Za-z_0-9]{10,}").matcher(password);
-
-        return matcher.matches();
+        return Pattern.compile("^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])[A-Za-z_0-9]{10,}").matcher(password).matches();
     }
 }
